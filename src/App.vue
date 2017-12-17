b69b54080b2683d2301f37a29610e98de0eeede9
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2017-12-17 19:44:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <date-range-picker
+        @apply="onDateChange"
+        opens="left"
+    >
+    </date-range-picker>
+  </div>
+</template>
+
+<script>
+import DateRangePicker from './components/DateRangePicker.vue'
+
+export default {
+  name: 'app',
+  components: {DateRangePicker},
+  data () {
+    return {
+      start: null,
+      end: null
+    }
+  },
+  methods: {
+    onDateChange (start, end) {
+      this.start = start
+      this.end = end
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
